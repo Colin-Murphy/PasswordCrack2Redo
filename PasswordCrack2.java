cc4211554a73cc503fbfd6c5b9cf012a7561b214
Colin Murphy <colinmurphy1993@gmail.com>
2014-03-28 16:45:49 -0430

    Implemented basically everthing. Known bugs: password collisions are not handled, and the counter doesnt work.

--- PasswordCrack2.java
+++ PasswordCrack2.java
@@ -1,45 +1,147 @@
+/**
+	File: PasswordCrack.java	
+	Designed for RIT Concepts of Paralel and Distributed Systems Project 1
+	
+	@author Colin L Murphy <clm3888@rit.edu>
+	@version 3/28/14
+*/
+
+//Read files
+import java.io.File;
+//Arraylist for storing users and hashed passwords
+import java.util.ArrayList;
+//Buffered reader
+import java.io.BufferedReader;
+//File reader
+import java.io.FileReader;
+//Io exception
+import java.io.IOException;
+//File not found
+import java.io.FileNotFoundException;
+
+
+//Hex Class
+import edu.rit.util.Hex;
+
+//Users are contained in a hashmap
+import java.util.*;
+
+
 import edu.rit.pj2.IntVbl;
 import edu.rit.pj2.Loop;
 import edu.rit.pj2.Task;
 
+//Hash passwords
+import java.security.MessageDigest;
+
 /**
  * Class PasswordCrack2 is the password cracking main program.
  */
-public class PasswordCrack2
-    extends Task
-    {
+public class PasswordCrack2 extends Task {
     /**
      * Global reduction variable for number of passwords found.
      */
     IntVbl found;
+    
+    //Hashmap to contain the users and their hashed passwords
+	private HashMap<String, String> users = new HashMap<String, String>();
 
     /**
      * Main program.
      *
      * @param  args  Command line argument strings.
      */
-    public void main
-        (String[] args)
-        throws Exception
-        {
-        // TBD
+    public void main(String[] args) throws Exception {
+        //Insure proper amount of arguments
+		if (args.length != 1) {
+			System.err.println("Usage: java pj2 PasswordCrack2" 
+				+ "<databaseFile>");
+			//Were done here
+			return;
+		}
+		
+		
+		File databaseFile = new File(args[0]);
+		
+		//Reader for reading the input file
+		BufferedReader reader;
 
-        parallelFor (0, 1727603) .exec (new Loop()
-            {
+		//Read the database
+		try {
+			reader = new BufferedReader
+				(new FileReader(databaseFile));
+		}
+		
+		//File not found, tell the user and give up
+		catch(FileNotFoundException e) {
+			System.err.println("File " + databaseFile.getName() + 
+				" does not exist");
+			System.exit (1);
+			//End the program
+			return;
+		}
+
+		
+		String line = "";
+		//Parse input and start thread group 2 (users)
+		try {
+			while((line = reader.readLine()) !=null) {
+				//Split into tokens at any whitespace
+				String[] tokens = line.split("\\s+");
+				
+				//Add this password and use to the hashmap
+				users.put(tokens[1], tokens[0]);
+
+				
+			}
+		}
+		
+		
+		//Something went wrong, give up
+		catch(IOException e) {
+			System.err.println("Error reading " + 
+				databaseFile.getName());
+			System.exit (1);
+		}
+
+		found = new IntVbl.Sum(0);
+        parallelFor (0, 1727603) .exec (new Loop() {
             // Thread-local reduction variable for number of passwords found.
             IntVbl thrFound;
 
             // TBD
 
-            public void run (int i) throws Exception
-                {
+            public void run (int i) throws Exception {
                 String password = getPassword (i);
-                // TBD
-                }
-            });
+                thrFound = threadLocal(found);
+                //Get the password hash of the index in base 36 as a string
+                
+                byte[] data;
+                try {
+					MessageDigest md = MessageDigest.getInstance ("SHA-256");
+					data = password.getBytes ("UTF-8");
+					md.update (data);
+					data = md.digest();
+				}
+				catch (Throwable exc) {
+					throw new IllegalStateException ("Shouldn't happen", exc);
+				}
 
-        // TBD
-        }
+				String hex = Hex.toString(data);
+				
+				//If this hash exists then print it
+				if (users.containsKey(hex)) {
+					System.out.println(users.get(hex) + " " + password);
+					thrFound.item++;
+				}
+			}
+		});
+		
+		//Print the statistics
+		System.out.println(users.size() + " users");
+		System.out.println(found.item + " passwords found");
+
+	}
 
     /**
      * Get the password for the given index.
@@ -48,35 +150,47 @@ public class PasswordCrack2
      *
      * @return  Password corresponding to index.
      */
-    private static String getPassword
-        (int index)
-        {
-        // TBD
-
-        if (index < 36)
-            {
-            // Convert index to one of 36 possible one-character passwords
-            // TBD
-            }
-        else if (index < 1332)
-            {
+    private static String getPassword(int index) {
+		//All the characters in the string combinations        
+        char[] chars = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d',
+        	'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
+        	'v','w','x','y','z'};
+        	
+		//less than 36 is really easy
+        if (index < 36) {
+            return ("" + chars[index]);
+		}
+        else if (index < 1332) {
             // Convert index to one of 36^2 = 1296 possible two-character
             // passwords
-            // TBD
-            }
-        else if (index < 47988)
-            {
+            char ones = chars[index%36];
+            int value = index/36;
+            char tens = chars[value%36];
+            return "" + tens + ones;
+		}
+        else if (index < 47988) {
             // Convert index to one of 36^3 = 46656 possible three-character
             // passwords
-            // TBD
-            }
-        else
-            {
+            char ones = chars[index%36];
+            int value = index / 36;
+            char tens = chars[value%36];
+            value = value/36;
+            char hundreds = chars[value%36];
+            
+            return "" + hundreds + tens + ones;
+		}
+        else {
             // Convert index to one of 36^4 = 1679616 possible four-character
             // passwords
-            // TBD
-            }
-
-        // TBD
+            char ones = chars[index%36];
+            int value = index / 36;
+            char tens = chars[(value%36)];
+            value = value/36;
+            char hundreds = chars[value%36];
+            value = value/36;
+            char thousands = chars[value%36];
+            
+            return "" + thousands + hundreds + tens + ones;
         }
-    }
+	}
+}
